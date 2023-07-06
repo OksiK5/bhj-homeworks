e0ffed2b54c723ff8f8b231069fde51298c494d1
KaminOks <oxi.k.nvrsk@mail.ru>
2023-07-06 22:12:21 +0300

    убираем лишнее

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,54 +1,14 @@
-(() => {
-	let playing = true,
-		activeHole = 1,
-		score = 0,
-		misses = 0;
+const holes = document.querySelectorAll(".hole");
+const hits = document.getElementById("dead");
+const misses = document.getElementById("lost");
 
-	const stop = () => playing = false,
-		getHole = index => document.getElementById(`hole${index}`),
-		deactivateHole = index => getHole(index).className = 'hole',
-		activateHole = index => getHole(index).className = 'hole hole_has-mole',
-		next = () => setTimeout(() => {
-			if (!playing) {
-				return;
-			}
-			deactivateHole(activeHole);
-			activeHole = Math.floor(1 + Math.random() * 9);
-			activateHole(activeHole);
-			next();
-		}, 800);
-
-	const moleClickHandler = (event) => {
-		if (event.target.classList.contains('hole_has-mole')) {
-			score++;
-			document.getElementById('dead').textContent = score;
-			if (score === 10) {
-				stop();
-				alert('Вы победили!');
-				score = 0;
-				misses = 0;
-				document.getElementById('dead').textContent = score;
-				document.getElementById('lost').textContent = misses;
-			}
-		} else {
-			misses++;
-			document.getElementById('lost').textContent = misses;
-			if (misses === 5) {
-				stop();
-				alert('Вы проиграли!');
-				// Обнуляем статистику
-				score = 0;
-				misses = 0;
-				document.getElementById('dead').textContent = score;
-				document.getElementById('lost').textContent = misses;
-			}
-		}
-	};
-
-	next();
-
-	const holes = document.querySelectorAll('.hole');
-	holes.forEach((hole) => {
-		hole.addEventListener('click', moleClickHandler);
-	});
-})();
+holes.forEach(hole => {
+  hole.onclick = function () {
+    hole.className.includes("hole_has-mole") ? hits.textContent++ : misses.textContent++;
+    if (parseInt(hits.textContent) === 10 || parseInt(misses.textContent) === 5) {
+      alert(parseInt(hits.textContent) === 10 ? "Вы выиграли!" : "Вы проиграли!");
+      hits.textContent = 0;
+      misses.textContent = 0;
+     } 
+   }
+ });
