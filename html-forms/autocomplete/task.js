d4940060baa3118f7a0d8e9284a306ca92cac43f
KaminOks <oxi.k.nvrsk@mail.ru>
2023-07-21 17:38:14 +0300

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -67,26 +67,19 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+  getMatches(text) {
+    const matches = [];
+    const options = Array.from(this.input.options);
+    options.forEach((option) => {
+      if (option.innerHTML.includes(text)) {
+        matches.push({
+          text: option.innerHTML,
+          value: option.value,
+        });
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    });
+  
+    return matches;
   }
 }
 
